4f2d0f1601cb339c74539ed795ad85016c51d617
Nasirhussain4 <nasirhussain805864@gmail.com>
2025-05-08 09:52:36 +0530

    Create scrift.js

---
+++ scrift.js
@@ -0,0 +1,10 @@
+function toggleMobileMenu() {
+    const links = document.getElementById("nav-links");
+    links.classList.toggle("show");
+  }
+  
+  function toggleDropdown() {
+    const dropdown = document.getElementById("dropdown");
+    dropdown.classList.toggle("show");
+  }
+  
